0a67a1221cb0b3e0294340063777c2923285ff88
polegut <polegut8@gmail.com>
2023-07-30 06:46:17 +0300

    Add project files.

---
+++ MOBİLE BANK/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MOBİLE_BANK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
